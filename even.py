f27df36dd7998ed55945793ced95ad4075d5f2f7
MGillOU <35855657+MGillOU@users.noreply.github.com>
2019-03-10 21:12:39 +0000

    Update and rename prime.py to even.py

---
+++ even.py
@@ -0,0 +1,9 @@
+# Python program to check if the number is even
+
+tnumber = 407
+
+# number is even if num % 2 == 0
+if (tnumber % 2) == 0:
+   print("{0} is an even number".format(tnumber))
+else:
+   print("{0} is an odd number".format(tnumber))
